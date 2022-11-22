a187fc1278f6c34363975a4632ca568a84dc3c9f
M-Cleveland153 <m999cleveland@gmail.com>
2022-11-21 21:26:26 -0600

    9:26pm

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { Component } from "react";
 
-const Game = () => {
-  return (
-    <div>Game</div>
-  )
+class Game extends Component {
+
+  
+  constructor(props) {
+    super(props);
+    this.state = { checked: 0,
+    artists: [
+      {name: "Citizen Cope", id: '1'},
+      {name: "Ludovico", id: '2'},
+      {name: "Led Z", id: '3'},
+    ] };
+  }
+
+  onChange(i) {
+    this.setState({
+      checked: 0,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.artists.map((option, i) => {
+          return (
+            <label key={option.id}>
+              <input
+                type="radio"
+                className={this.props.className}
+                checked={this.state.checked == i ? true : false}
+                key={option.id}
+                onChange={this.onChange}
+                value={option.name}
+              />
+              {option.name}
+            </label>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
-export default Game
+export default Game;
+
